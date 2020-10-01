c1af2d2c48b2fdeb772429cf17877d4dd4e7ca8d
Aurelien-Brison <brison.aurelien@gmail.com>
2020-10-01 13:59:16 +0200

    add fixture + add ApiPlatform Annotation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,49 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Article;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private UserPasswordEncoderInterface $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
+
+        $fake = Factory::create();
+
+        for($u = 0 ; $u < 10 ; $u++) {
+
+            $user = new User();
+            $passHash = $this->encoder->encodePassword($user, 'password');
+            $user->setEmail($fake->email)
+                ->setPassword($passHash)
+                ;
+
+            $manager->persist($user);
+
+            for($a = 0 ; $a < random_int(5, 15) ; $a++ ) {
+                $article = (new Article())->setAuthor($user)
+                ->setContent($fake->text(300))
+                ->setName($fake->name(50))
+                ;
+
+                $manager->persist($article);
+            }
+        }
+
 
         $manager->flush();
     }
