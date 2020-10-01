8471a722f2d1b25f74cff9d98e8db513098a8bea
Aurelien-Brison <brison.aurelien@gmail.com>
2020-10-01 11:46:33 +0200

    init project
    add packages && implement entities

---
+++ src/Entity/Timestapable.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace App\Entity;
+
+trait Timestapable
+{
+    /**
+     * @var \DateTimeInterface
+     * @ORM\Column(type="datetime")
+     */
+    private \DateTimeInterface $createdAt;
+
+    /**
+     * @var \DateTimeInterface
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private \DateTimeInterface $updatedAt;
+
+    /**
+     * Get the value of createdAt
+     *
+     * @return  \DateTimeInterface
+     */ 
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Get the value of updatedAt
+     *
+     * @return  \DateTimeInterface
+     */ 
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set the value of updatedAt
+     *
+     * @param  \DateTimeInterface  $updatedAt
+     *
+     * @return  self
+     */ 
+    public function setUpdatedAt(\DateTimeInterface $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
